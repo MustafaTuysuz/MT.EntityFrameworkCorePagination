3146132082f47678aea750b863b22a9885eb1a53
Mustafa Tüysüz <113853040+MustafaTuysuz@users.noreply.github.com>
2025-07-20 16:22:54 +0300

    Add project files.

---
+++ MT.EntityFrameworkCorePagination/Pagination/PagedListQueryableExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MT.EntityFrameworkCorePagination.Pagination;
+
+public static class PagedListQueryableExtensions
+{
+    public static async Task<PaginationResult<T>> ToPagedListAsync<T>(
+        this IQueryable<T> source,
+        int pageNumber,
+        int pageSize)
+    {
+        var count = await source.CountAsync();
+
+        if (count > 0)
+        {
+            var items = await source
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return new PaginationResult<T>(items, pageNumber, pageSize, count);
+        }
+
+        return new PaginationResult<T>(new List<T>(), 0, 0, 0);
+    }
+}
---
+++ MT.EntityFrameworkCorePagination/Pagination/PaginationResult.cs
@@ -0,0 +1,21 @@
+﻿namespace MT.EntityFrameworkCorePagination.Pagination;
+
+public class PaginationResult<T>
+{
+    public PaginationResult(IList<T> items, int pageNumber, int pageSize, int totalCount)
+    {
+        Items = items;
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+        TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+        IsFirstPage = pageNumber == 1;
+        IsLastPage = pageNumber == TotalPages;
+    }
+
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+    public int TotalPages { get; set; }
+    public IList<T> Items { get; set; }
+    public bool IsFirstPage { get; set; }
+    public bool IsLastPage { get; set; }
+}
